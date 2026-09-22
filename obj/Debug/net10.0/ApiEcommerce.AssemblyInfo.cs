7da08daa39311e96cf7930ec6f9ab2a413c36d71
Emanuel Gonzalez <eg554848@gmail.coml>
2026-09-22 17:19:05 -0600

    Collection completa de las rutas en postman

--- obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
+++ obj/Debug/net10.0/ApiEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff23aa9aff74657a1e5444c7f1214e1fbd75674d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53dc6c60e25a2edb524ae010d33c9ca4875c9086")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
